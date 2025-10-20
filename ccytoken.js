1367fd570e1d9ff561fd649213c1b019f4751e50
qq24163 <2416324163@qq.com>
2025-10-20 15:01:14 +0800

    Create ccytoken.js

---
+++ ccytoken.js
@@ -0,0 +1,9 @@
+/*
+-------------- Quantumult X 配置 --------------
+
+[MITM]
+hostname = lmf.lvmifo.com
+
+[rewrite_local]
+^https:\/\/lmf\.lvmifo\.com\/api\/5dca57afa379e.*m=getIntegralLog url script-request-header https://raw.githubusercontent.com/qq24163/hq/refs/heads/main/lmftoken.js
+*/
