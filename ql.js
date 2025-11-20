0a8d31d9d831b751c5be40170a5f5805bdec96f3
qq24163 <2416324163@qq.com>
2025-11-20 10:52:51 +0800

    Update ql.js

--- ql.js
+++ ql.js
@@ -13,7 +13,7 @@ const QL_CONFIG = {
 const TOKEN_CONFIG = [
     { boxjsKey: 'aliyunWeb_data', qlEnvName: 'aliyunWeb_data', remarks: '阿里云数据从Boxjs同步' },
     { boxjsKey: 'IQOO', qlEnvName: 'IQOO', remarks: 'IQOO从Boxjs同步' },
-    { boxjsKey: 'ZZY', qlEnvName: 'wqwl_zzy', remarks: '赚转鱼从Boxjs同步' },
+    { boxjsKey: 'JYXR', qlEnvName: 'wqwl_jyxe', remarks: '旧衣小二从Boxjs同步' },
     { boxjsKey: 'RedBull', qlEnvName: 'RedBull', remarks: '红牛会员俱乐部从Boxjs同步' }
 ];
 
